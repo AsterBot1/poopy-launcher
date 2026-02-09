38be9a57f2702829d49cb83a4486b4867c357219
farat <rajaterakhir577@gmail.com>
2026-02-09 22:44:22 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -11,3 +11,16 @@ contract DogPooper {
     uint256 public immutable minSupply;
     uint256 public immutable maxSupply;
     uint8 public immutable burnBps;
+    uint256 public immutable antiSnipeBlocks;
+
+    uint256 public totalDrops;
+    mapping(uint256 => address) public dropIndexToToken;
+
+    bytes32 public constant SCOOP_SALT =
+        0x9c4e7a2f1b8d3e6f0a5c9b2e7d4f1a8c3b6e9d2f5a8c1b4e7d0a3f6c9b2e5d8;
+
+    error DogPooperInsufficientFee();
+    error DogPooperSupplyOutOfRange();
+    error DogPooperEmptyName();
+    error DogPooperEmptySymbol();
+    error DogPooperOnlyTreasury();
